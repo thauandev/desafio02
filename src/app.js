f20415a7a5792857f2cc2e72768a4f8d4958b551
thauandev <thauandepaula@icloud.com>
2020-04-27 10:22:56 -0300

    Desafio feito

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,25 +9,88 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const likes = 0;
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const repositorie = repositories
+  
+  return response.json(repositorie);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+ const { title, url, techs } = request.body;
+
+ const repositorie = { id: uuid(), title, url, techs, likes }
+
+ repositories.push(repositorie);
+
+ return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url , techs } = request.body;
+
+  const { id } = request.params;
+
+ repostiorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+ if (repostiorieIndex < 0) {
+   return response.status(400).json({ error: 'ID is invalid'});
+ }
+
+const repositorie = {
+  id, 
+  title, 
+  url, 
+  techs, 
+  likes
+}
+
+repositories[repostiorieIndex] = repositorie;
+
+return response.json(repositorie);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+ repostiorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+ if (repostiorieIndex < 0) {
+   return response.status(400).json({ error: 'ID is invalid'});
+ }
+ repositories.splice(repostiorieIndex, 1);
+
+ return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+ repostiorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+ if (repostiorieIndex < 0) {
+   return response.status(400).json({ error: 'ID is invalid'});
+ }
+ 
+ 
+  repositories[repostiorieIndex].likes += 1;
+
+
+repositories[repostiorieIndex] = {
+  id: repositories[repostiorieIndex].id, 
+  title: repositories[repostiorieIndex].title, 
+  url: repositories[repostiorieIndex].url, 
+  techs: repositories[repostiorieIndex].techs, 
+  likes: repositories[repostiorieIndex].likes,
+};
+
+return response.json({
+  title: repositories[repostiorieIndex].title, 
+  likes: repositories[repostiorieIndex].likes,
+});
+
 });
 
 module.exports = app;
